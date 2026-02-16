5aacfb51e184241505fd7c55a09c5f77e5f7503e
notadmg <157731068+notadmg@users.noreply.github.com>
2026-02-16 19:25:54 +0400

    added my new mod

---
+++ mods/bettersmoothliquids.js
@@ -0,0 +1,37 @@
+//bettersmoothliquids.js mod for sandboxels by nota
+//water
+elements.water.behavior = [
+    "XX|XX|XX",
+    "M2%5|XX|M2 AND BO",
+    "XX|M1|M2",
+];
+elements.water.flippableX = true;
+//salt_water
+elements.salt_water.behavior = [
+    "XX|XX|XX",
+    "M2%5|XX|M2 AND BO",
+    "XX|M1|M2",
+];
+elements.salt_water.flippableX = true;
+//seltzer
+elements.seltzer.behavior = [
+    "XX|XX|XX",
+    "M2%5|XX|M2 AND BO",
+    "XX|M1|M2",
+];
+elements.seltzer.flippableX = true;
+//sugar_water
+elements.sugar_water.behavior = [
+    "XX|XX|XX",
+    "M2%5|XX|M2 AND BO",
+    "XX|M1|M2",
+];
+elements.sugar_water.flippableX = true;
+//pool_water
+elements.pool_water.behavior = [
+    "XX|XX|XX",
+    "M2%5|XX|M2 AND BO",
+    "XX|M1|M2",
+];
+elements.pool_water.flippableX = true;
+console.log('bettersmoothliquids.js successfully loaded');
